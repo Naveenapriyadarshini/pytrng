54e168369574ba4851c896a0708f35b4876e2010
unknown <Administrator@BG4WS0866.SONATA.LOCAL>
2021-09-28 14:19:14 +0530

    practice work 1 done

---
+++ hello1.py
@@ -0,0 +1,9 @@
+a = int(input("Enter 1st no"))
+b = int(input ("Enter 2nd no"))
+c = int(input ("Enter 3rd no"))
+if(a>b & a>c):
+    print("a is biggest")
+elif(b>a & b>c):
+    print("b is biggest")
+else:
+    print("c is biggest")
---
+++ hello2.py
@@ -0,0 +1,4 @@
+n = str(input("Enter name"))
+g = int(input ("Enter age"))
+year = str((2021 - g)+100)
+print(n +' ' + "will turn 100 years old in the year "+ year)
